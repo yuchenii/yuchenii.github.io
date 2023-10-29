88379c0178ebae7e0130087e021aede4f5467bde
yuchen <dev.chenyu@gmail.com>
2023-10-29 23:40:25 +0800

    chore: 更新主题版本

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -147,25 +147,49 @@ export default hopeTheme(
       mdEnhance: {
         align: true,
         attrs: true,
+        card: true,
+  
+        // install chart.js before enabling it
         chart: true,
+  
         codetabs: true,
         demo: true,
+  
+        // install echarts before enabling it
         echarts: true,
+  
         figure: true,
+  
+        // install flowchart.ts before enabling it
         flowchart: true,
+  
         gfm: true,
         imgLazyload: true,
         imgSize: true,
         include: true,
-        katex: true,
+  
+        // katex 与 mathjax选择一个启用
+        // install katex before enabling it
+        // katex: true,
+  
+        // install mathjax-full before enabling it
+        mathjax: true,
+  
         mark: true,
+  
+        // install mermaid before enabling it
         mermaid: true,
+  
         playground: {
           presets: ["ts", "vue"],
         },
-        revealjs: {
+  
+        // 幻灯片
+        // install reveal.js before enabling it
+        revealJs: {
           plugins: ["highlight", "math", "search", "notes", "zoom"],
         },
+  
         stylize: [
           {
             matcher: "Recommended",
@@ -183,6 +207,8 @@ export default hopeTheme(
         sup: true,
         tabs: true,
         vPre: true,
+  
+        // install @vue/repl before enabling it
         vuePlayground: true,
       },
 
