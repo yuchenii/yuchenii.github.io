33ae2e9d9ec3004be008b2b0fd1568ee59fe4e83
yuchen <dev.chenyu@gmail.com>
2023-10-31 08:43:07 +0800

    feat: 修改了首页一言打字效果

--- src/.vuepress/components/Theme/BlogHero.vue
+++ src/.vuepress/components/Theme/BlogHero.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
 import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
-import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+// import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+import HitokotoBlogHero from "./HitokotoBlogHero.js";
 import { computed } from "vue";
 
 const filteredProps = computed(() => {
