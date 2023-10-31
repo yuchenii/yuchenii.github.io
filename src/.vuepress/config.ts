33c327e2dabca3597067a7aa15786dd19253960a
yuchen <dev.chenyu@gmail.com>
2023-11-01 00:18:47 +0800

    feat: 使用Algolia作为搜索工具

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { getDirname, path } from "@vuepress/utils";
 import { defineUserConfig } from "vuepress";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
+// import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 import theme from "./theme.js";
 const __dirname = getDirname(import.meta.url);
@@ -30,21 +31,72 @@ export default defineUserConfig({
   theme,
 
   plugins: [
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
+    docsearchPlugin({
+      // 你的选项
+      // appId, apiKey 和 indexName 是必填的
+      appId: '7EMNMOAKZW',
+      apiKey: 'b0fc524652dfa982328001adcd02de22',
+      indexName: 'yuchen',
+
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
         },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
+      },
     }),
+    // searchProPlugin({
+    //   // 索引全部内容
+    //   indexContent: true,
+    //   // 为分类和标签添加索引
+    //   customFields: [
+    //     {
+    //       getter: (page) => page.frontmatter.category,
+    //       formatter: "分类：$content",
+    //     },
+    //     {
+    //       getter: (page) => page.frontmatter.tag,
+    //       formatter: "标签：$content",
+    //     },
+    //   ],
+    // }),
   ],
 
   alias: {
